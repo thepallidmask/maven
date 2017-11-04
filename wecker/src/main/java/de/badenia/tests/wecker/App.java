c9a951e4ce0094955d84077b671202b046bd382a
Niko <Niko@DESKTOP-MO19DGA.local>
2017-11-04 13:01:27 +0100

    Initial

---
+++ wecker/src/main/java/de/badenia/tests/wecker/App.java
@@ -0,0 +1,13 @@
+package de.badenia.tests.wecker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
